38d9ce64d65320ab045aa1e6b985721bf8c81032
Giovanni Sakti <giosakti@starqle.com>
2017-06-30 07:51:22 +0700

    use method for generating connection string

--- system/boot/persistence.rb
+++ system/boot/persistence.rb
@@ -8,21 +8,7 @@ module RopExample
 
       # Setup connection string
       db_config = db_config[Application.config.env]
-      conn_string = db_config['adapter']
-
-      unless db_config['host'].blank?
-        host_string = db_config['host']
-        host_string = "#{host_string}:#{db_config['port']}" unless db_config['port'].blank?
-
-        unless db_config['username'].blank?
-          user_string = db_config['username']
-          user_string = "#{user_string}:#{db_config['password']}" unless db_config['password'].blank?
-          host_string = "#{user_string}@#{host_string}"
-        end
-
-        conn_string = "#{conn_string}://#{host_string}"
-        conn_string = "#{conn_string}/#{db_config['database']}" unless db_config['database'].blank?
-      end
+      conn_string = Application.generate_connection_string(db_config)
 
       # Initiate db connection
       db = ::ROM.container(:sql, conn_string) do |conf|
