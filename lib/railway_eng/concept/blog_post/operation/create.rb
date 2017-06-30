534d17aea6f1a16a28986719422bb4dcc13e4b9b
Giovanni Sakti <giosakti@gmail.com>
2017-06-30 22:29:49 +0700

    fix error when starting rspec

--- lib/railway_eng/concept/blog_post/operation/create.rb
+++ lib/railway_eng/concept/blog_post/operation/create.rb
@@ -2,39 +2,41 @@ require 'import'
 
 module RailwayEng
   module Concept
-    class BlogPost::Create < Pelita::Operation::Base
-      include Import["blog_post_repo"]
+    module BlogPost
+      class Create < Pelita::Operation::Base
+        include Import["blog_post_repo"]
 
-      step :config!
-      step :authorize!
-      step :validate!
-      attempt :persist!, catch: StandardError
-      step :notify!
+        step :config!
+        step :authorize!
+        step :validate!
+        attempt :persist!, catch: StandardError
+        step :notify!
 
-      # dry-transaction does not allow arguments to be passed on the
-      # DSL steps. Add extra step instead and pass arguments via options
-      def config!(options)
-        Right(options)
-      end
+        # dry-transaction does not allow arguments to be passed on the
+        # DSL steps. Add extra step instead and pass arguments via options
+        def config!(options)
+          Right(options)
+        end
 
-      def validate!(options)
-        params = options["params"]
-        result = BlogPost::Contract::Create.call(params[:blog_post])
-        options["result.contract.default"] = result
+        def validate!(options)
+          params = options["params"]
+          result = BlogPost::Contract::Create.call(params[:blog_post])
+          options["result.contract.default"] = result
 
-        result.success? ? Right(options) : Left(options)
-      end
+          result.success? ? Right(options) : Left(options)
+        end
 
-      def persist!(options)
-        params = options["params"]
-        options["model"] = Entity::BlogPost.new(params[:blog_post])
-        options["model"] = blog_post_repo.create(options["model"])
-        Right(options)
-      end
+        def persist!(options)
+          params = options["params"]
+          options["model"] = Entity::BlogPost.new(params[:blog_post])
+          options["model"] = blog_post_repo.create(options["model"])
+          Right(options)
+        end
 
-      def notify!(options)
-        # BlogPost::Notification.(current_user, model)
-        Right(options)
+        def notify!(options)
+          # BlogPost::Notification.(current_user, model)
+          Right(options)
+        end
       end
     end
   end
